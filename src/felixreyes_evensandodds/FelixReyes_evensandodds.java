ea8c54930fce102434e5350040172154b3396a82
aflynn <aflynn@ninja11.d22.lan>
2015-09-30 07:31:21 -0500

    Felix

---
+++ src/felixreyes_evensandodds/FelixReyes_evensandodds.java
@@ -0,0 +1,36 @@
+package felixreyes_evensandodds;
+public class FelixReyes_evensandodds {
+    
+    public static void main(String[] args) {
+//        int counter =0;
+//        //counter command goes outside
+//        
+//       for(int i = 0; i < 100; i++) {
+//            if(i % 2 == 0 ) {
+//                counter++;
+//                System.out.println("this is a counter"
+//                        + "in the forloop: " + counter);
+//            }
+//        } System.out.println ("the final count is..." + counter);
+//        for(int i = 0; i < 100; i ++){
+//            if(isPrime(i)){
+//                System.out.println(i + "is prime");
+//            }
+//        
+//        
+        /* 
+            Method- is like a program,
+            it can receive and return values to other methods
+            */
+   }
+
+//            static boolean isPrime(int n){
+//                return(n % 2 == 0);
+//            }
+//            int devisor = 0; 
+//            for(int i = 2; i < p;i ++){
+//            return (n % 2 == i);
+    }
+        
+    
+//    }
